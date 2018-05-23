046b4ee6d7a8a8a4146c34713a6a43bf059fadc9
Shelly Zhao <shellyzhao001@gmail.com>
2018-05-23 08:43:15 +0800

    完成未来24小时天气获取，使用udacity测试接口，以及完成代码重构

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,7 +21,7 @@ Page({
     nowTemp: '',
     nowWeather: '',
     nowWeatherBackground: '',
-    forecast: [],
+    hourlyWeather: [],
   },
   onPullDownRefresh(){
     this.getNow(()=>{
@@ -31,27 +31,25 @@ Page({
       wx.stopPullDownRefresh()
     })
   },
-  onLoad(){
-    // console.log("Hello World");
-    
+  onLoad(){ 
     this.getNow();
     // onLoad中没有传入回调函数，所以不会执行停止下拉刷新
 
-    wx.request({
-      url: 'https://api.seniverse.com/v3/weather/daily.json?key=j03ff2ziir99zle2&language=zh-Hans&unit=c&start=0&days=5', //调用心知天气的免费天气预报Api，可返回三天的天气预报
-      data: {
-        // 修改城市为成都
-        location: 'chengdu',
+  //   wx.request({
+  //     url: 'https://api.seniverse.com/v3/weather/daily.json?key=j03ff2ziir99zle2&language=zh-Hans&unit=c&start=0&days=5', //调用心知天气的免费天气预报Api，可返回三天的天气预报
+  //     data: {
+  //       // 修改城市为成都
+  //       location: 'chengdu',
 
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: res => {
-        console.log(res)
+  //     },
+  //     header: {
+  //       'content-type': 'application/json' // 默认值
+  //     },
+  //     success: res => {
+  //       console.log(res)
 
-      }
-    })
+  //     }
+  //   })
   },
   getNow(callback){
     // getNow传入回调函数callback
@@ -68,40 +66,73 @@ Page({
       success: res => {
         console.log(res)
         let result = res.data.results[0]
-        let temp = result.now.temperature
-        let weather = result.now.text
-        console.log(temp, weather)
-        this.setData({
-          nowTemp: temp,
-          nowWeather: weather,
-          nowWeatherBackground: '/images/' + weatherMap[weather] + '-bg.png'
-
-        })
-        //set forcast
-        let forecast = []
-        let newHour = new Date().getHours()
-        for (let i = 0; i < 24; i += 3) {
-          forecast.push({
-            //取当前时间每隔三小时的预报，超过24小时取余
-            time: (i + newHour) % 24 + '时',
-            iconPath: '/images/sunny-icon.png',
-            temp: '12°'
-
-          })
-        }
-        forecast[0] = 
-        this.setData({
-          forecast: forecast
-        })
-        wx.setNavigationBarColor({
-          frontColor: '#000000',
-          backgroundColor: weatherColorMap[weather],
-        })
+        this.setNow(result)
       },
       complete: ()=> {
         // 有回调函数传入，执行callback
         callback && callback()
       }
     });
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '成都市'
+      },
+      success: res => {
+        console.log(res)
+        let forcastResult = res.data.result
+        this.setHourlyWeather(forcastResult)
+        // let temp = result.now.temp
+        // let weather = result.now.weather
+        // this.setData({
+        //   nowTemp: temp + '°',
+        //   nowWeather: weatherMap[weather],
+        //   nowWeatherBackground: '/images/' + weather + '-bg.png'
+        // })
+        // wx.setNavigationBarColor({
+        //   frontColor: '#000000',
+        //   backgroundColor: weatherColorMap[weather],
+        // })
+
+        //set hourlyWeather
+
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  },
+  //代码重构，将getNow函数里面success的回调函数，设置天气的部分拆分成setNow和setHourlyWeather，分别代表设置当前天气和设置每小时天气，这样比较清晰，success的回调函数就只用获取天气就行了
+  setNow(result) {
+    let temp = result.now.temperature
+    let weather = result.now.text
+    console.log(temp, weather)
+    this.setData({
+      nowTemp: temp,
+      nowWeather: weather,
+      nowWeatherBackground: '/images/' + weatherMap[weather] + '-bg.png'
+
+    }),
+      wx.setNavigationBarColor({
+        frontColor: '#000000',
+        backgroundColor: weatherColorMap[weather],
+      })
+  },
+  setHourlyWeather(forcastResult) {
+    let forecast = forcastResult.forecast
+    let hourlyWeather = []
+    let nowHour = new Date().getHours()
+    for (let i = 0; i < 8; i += 1) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + "时",
+        iconPath: '/images/' + forecast[i].weather + '-icon.png',
+        temp: forecast[i].temp + '°'
+      })
+    }
+    hourlyWeather[0].time = '现在'
+    this.setData({
+      hourlyWeather: hourlyWeather
+    })
+
   }
 })
